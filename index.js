aa75623449b49c7860ca9b02a3f7b187cc857102
timzo31 <69592997+timzo31@users.noreply.github.com>
2021-10-31 14:08:28 +0800

    Mongoose Part 2

--- index.js
+++ index.js
@@ -8,13 +8,10 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
   console.log("Conneted correctly to the server");
 
-  let newDish = Dishes({
+  Dishes.create({
     name: "Uthappizza",
     description: "test",
-  });
-
-  newDish
-    .save()
+  })
     .then((dish) => {
       console.log(dish);
 
@@ -23,7 +20,7 @@ connect.then((db) => {
     .then((dishes) => {
       console.log(dishes);
 
-      return Dishes.remove({}).exec();
+      return Dishes.deleteOne({});
     })
     .then(() => {
       return mongoose.connection.close();
@@ -32,3 +29,31 @@ connect.then((db) => {
       console.log(err);
     });
 });
+
+// connect.then((db) => {
+//   console.log("Conneted correctly to the server");
+
+//   let newDish = Dishes({
+//     name: "Uthappizza",
+//     description: "test",
+//   });
+
+//   newDish
+//     .save()
+//     .then((dish) => {
+//       console.log(dish);
+
+//       return Dishes.find({}).exec();
+//     })
+//     .then((dishes) => {
+//       console.log(dishes);
+
+//       return Dishes.remove({}).exec();
+//     })
+//     .then(() => {
+//       return mongoose.connection.close();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
